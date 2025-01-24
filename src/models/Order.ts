8b3eb6bccfe201ac4c98d750f7cf2d9336c964dd
Isihaq abdullahi <isihaqabdullahi01@gmail.com>
2025-01-24 21:35:41 +0100

    feat: Order Schema

---
+++ src/models/Order.ts
@@ -0,0 +1,23 @@
+import mongoose, { mongo, Schema } from "mongoose";
+import { OrderType } from "../types/types";
+
+const OrderSchema = new Schema({
+    user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    Products: [{
+        Product: { type: Schema.Types.ObjectId, ref: "Product", required: true },
+        quatity: { type: Number, required: true }
+    }],
+    total_price: { type: Number, required: true },
+    status: { type: String, enum: ["pending", "processing", "shiped", "delivered"], required: true },
+    address: { type: Schema.Types.ObjectId, ref: "Address", required: true }
+},
+{
+    timestamps: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+const Order = mongoose.model<OrderType>("Orders", OrderSchema)
+
+export default Order;
