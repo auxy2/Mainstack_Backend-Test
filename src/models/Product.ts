c0377650dc6bb76c9c44c3a69cd486c86c83d7c6
Isihaq abdullahi <isihaqabdullahi01@gmail.com>
2025-01-24 21:17:13 +0100

    feat: Product Schema

---
+++ src/models/Product.ts
@@ -0,0 +1,23 @@
+import mongoose, { Schema } from "mongoose";
+import { ProductType } from "../types/types";
+
+const ProductSchema = new Schema({
+    name: { type: String, required: true },
+    price: { type: Number, required: true },
+    describtion: { type: String, required: true },
+    images: [{ type: String, required: true }],
+    category: [{ type: String, required: true }],
+    color: [{ type: String, required: false }],
+    size: [{ type: String, required: false }],
+},
+{
+    timestamps: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+
+const Product = mongoose.model<ProductType>("Product", ProductSchema);
+
+export default Product;
